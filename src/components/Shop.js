e268c2cd96f00be8515a07cd63695eb1bcc4bb5b
Taylor W <taylorjwill09@gmail.com>
2022-12-05 22:22:23 -0500

    Setting flex on Shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,18 +2,20 @@ import "./Shop.css";
 
 function Shop() {
   return (
-    <div className="shop">
+    <>
       <h1>Shop</h1>
-      <div>
-        <h3 className="product1">product one</h3>
+      <div className="shop">
+        {/* <div> */}
+        <h3 className="product">product one</h3>
+        {/* </div> */}
+        {/* <div> */}
+        <h3 className="product">product two</h3>
+        {/* </div> */}
+        {/* <div> */}
+        <h3 className="product">product three</h3>
+        {/* </div> */}
       </div>
-      <div>
-        <h3 className="product2">product two</h3>
-      </div>
-      <div>
-        <h3 className="product3">product three</h3>
-      </div>
-    </div>
+    </>
   );
 }
 
