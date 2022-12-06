cfaecc7a60346b999e395b4b8535234d8f231db3
Taylor W <taylorjwill09@gmail.com>
2022-12-05 22:00:25 -0500

    Playing with flex

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,18 @@
+import "./Shop.css";
+
 function Shop() {
   return (
-    <div>
+    <div className="shop">
       <h1>Shop</h1>
+      <div>
+        <h3 className="product1">product one</h3>
+      </div>
+      <div>
+        <h3 className="product2">product two</h3>
+      </div>
+      <div>
+        <h3 className="product3">product three</h3>
+      </div>
     </div>
   );
 }
