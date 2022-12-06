762ac23f75405743602d175db8247f94124d455b
Taylor W <taylorjwill09@gmail.com>
2022-12-05 21:26:08 -0500

    Adds various sections to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,74 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home">
-      <h2>Home</h2>
-      <div className="mainSec">
-        <span>
-          Welcome to Plan+Flow
-          <br />
-          Build your life.
-          <br />
-          Organize your life
-          <br />
-          Plan+Flow
-        </span>
+    <>
+      <div className="home">
+        <h2>Home</h2>
+        <div className="mainSec">
+          <span>
+            Welcome to Plan+Flow
+            <br />
+            Build your life.
+            <br />
+            Organize your life
+            <br />
+            Plan+Flow
+          </span>
+        </div>
+        <div className="subSection">
+          <p className="subTitle">
+            The digital planner to help you organize your life!
+          </p>
+        </div>
+        <div className="lastSec">
+          <p className="subTitle">Get Started</p>
+        </div>
       </div>
-      <div className="subSection">
-        <p className="subTitle">
-          The digital planner to help you organize your life!
-        </p>
+      <div className="home">
+        <h2>Products</h2>
+        <div className="mainSec">
+          <span>
+            Welcome to Plan+Flow
+            <br />
+            Build your life.
+            <br />
+            Organize your life
+            <br />
+            Plan+Flow
+          </span>
+        </div>
+        <div className="subSection">
+          <p className="subTitle">
+            The digital planner to help you organize your life!
+          </p>
+        </div>
+        <div className="lastSec">
+          <p className="subTitle">Get Started</p>
+        </div>
       </div>
-      <div className="lastSec">
-        <p className="subTitle">Get Started</p>
+      <div className="home">
+        <h2>Making An Impact</h2>
+        <div className="mainSec">
+          <span>
+            Welcome to Plan+Flow
+            <br />
+            Build your life.
+            <br />
+            Organize your life
+            <br />
+            Plan+Flow
+          </span>
+        </div>
+        <div className="subSection">
+          <p className="subTitle">
+            The digital planner to help you organize your life!
+          </p>
+        </div>
+        <div className="lastSec">
+          <p className="subTitle">Get Started</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
